040cfcc9cb0bd3afc1451cdd5660ffc7eeb7e993
tarun <tarun@LAPTOP-384VCLUN>
2023-08-30 08:48:13 +0530

    Add project files.

---
+++ lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class pripub
+    {
+        private string t = "tarunkumar";
+        public string tar
+        {
+            get
+            {
+                return t;
+            }
+            set
+            {
+                t = value;
+            }
+        }
+    }
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            pripub ta = new pripub();
+            ta.tar = "kumar";
+            Console.WriteLine(ta.tar);
+        }
+    }
+}
